821a75404ae23773a2fec4f30a0fed0138de7fb0
TelmanovDmitry <telmanovdmitry@gmail.com>
2019-09-26 16:35:15 +0300

    Project creation

---
+++ app/src/main/java/com/example/itmo_android_2019_2020/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.itmo_android_2019_2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
